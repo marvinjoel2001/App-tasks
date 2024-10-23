0f657c4174b217703463e3a7cc3c7166f0d749e9
marvin <marvinjoelpena@gmail.com>
2024-10-22 20:18:40 -0400

    feat: v2.1-Add POST /tareas endpoint in TaskViewSet

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+    def create(self, request):
+        """Endpoint para crear una nueva tarea"""
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
