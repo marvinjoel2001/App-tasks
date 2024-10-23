d6a6ebe131cb332a2542558b9b80bb1db2829b04
marvin <marvinjoelpena@gmail.com>
2024-10-22 20:20:00 -0400

    feat: v2.2-Add DELETE /tareas/{id} endpoint in TaskViewSet

--- tareas/views.py
+++ tareas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
@@ -15,3 +15,11 @@ class TaskViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def destroy(self, request, pk=None):
+        """Endpoint para eliminar una tarea específica"""
+        try:
+            tarea = self.get_object()
+            tarea.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
